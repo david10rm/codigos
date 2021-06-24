b8520a871f268be52777f33fe28a8a64c321ac4d
david10rm <79180475+david10rm@users.noreply.github.com>
2021-06-24 17:24:58 -0500

    Add files via upload

---
+++ TareaHerencia/src/tareaherencia/UsarAlumno.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaherencia;
+
+import ico.fes.herencia.Alumno;
+
+/**
+ *
+ * @author adria
+ */
+public class UsarAlumno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno("318070836", "ICO", 2, "David", 20, 1, 1.70f);
+        System.out.println(alumno1);
+    }
+    
+}
